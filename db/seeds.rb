06737878275f4b2cdbc59c5d694cf4442339d5dd
Susanne Werner-Most <susanne.werner.most@gmail.com>
2019-11-12 19:57:49 +0100

    Rails Task Manager-add tasks entity

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create title: 'Laundry', details: 'Do not mix colors!'
+Task.create title: 'Studying', details: 'A lot of flashcards to do', completed: true
